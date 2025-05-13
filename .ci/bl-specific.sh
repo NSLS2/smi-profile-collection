0c68ce532f2fc94849ae836661db0f46457b10db
jennmald <jmaldonad@bnl.gov>
2025-05-13 16:48:51 -0400

    update prints for conda envs

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -3,6 +3,7 @@
 mkdir -p -v ~/.ipython/profile_test
 cp -v smi_config.csv ~/.ipython/profile_test/smi_config.csv
 
-conda env list
 conda activate $HOME/env
+conda env list
 python3 -m pip install --upgrade ophyd
+conda list
