db278a2584832e366f0e5877967d6cce2cc4154f
EliotGann <noemail@bnl.gov>
2025-05-14 12:53:19 -0400

    adding project to prompt prompt

--- startup/smibase/base.py
+++ startup/smibase/base.py
@@ -84,7 +84,7 @@ class ProposalIDPrompt(Prompts):
         return [
             (
                 Token.Prompt,
-                f"{RE.md.get('data_session', 'N/A')} [",
+                f"{RE.md.get('data_session', 'N/A')} {RE.md.get('project', 'N/A')} [",
             ),
             (Token.PromptNum, str(self.shell.execution_count)),
             (Token.Prompt, "]: "),
